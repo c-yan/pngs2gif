e67d315599b96ab41b20a4fabf8712a7e731c8e9
c-yan <c-yan@users.noreply.github.com>
2018-07-15 12:47:06 +0000

    Implemented filename filter (refs #1)

--- main.go
+++ main.go
@@ -4,8 +4,27 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 )
+
+func validateFileName(name string, startIndex int) bool {
+	t := strings.Split(name, ".")
+	if len(t) != 2 {
+		return false
+	}
+	i, err := strconv.Atoi(t[0])
+	if err != nil {
+		return false
+	}
+	if i < startIndex {
+		return false
+	}
+	if t[1] != "png" {
+		return false
+	}
+	return true
+}
 
 func listFiles(path string, startIndex int) ([]string, error) {
 	files, err := ioutil.ReadDir(path)
@@ -15,7 +34,10 @@ func listFiles(path string, startIndex int) ([]string, error) {
 
 	result := make([]string, 0, len(files))
 	for _, file := range files {
-		result = append(result, file.Name())
+		name := file.Name()
+		if validateFileName(name, startIndex) {
+			result = append(result, name)
+		}
 	}
 	return result, nil
 }
