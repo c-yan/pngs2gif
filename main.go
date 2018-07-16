7ed6026c3a7254bc593810742da30c7aeac3cdc1
c-yan <c-yan@users.noreply.github.com>
2018-07-16 13:57:42 +0000

    Added code to take CPU profile

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"image"
 	"image/color/palette"
 	"image/gif"
@@ -8,9 +9,24 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	inputDir := "wara"
 	startIndex := 13
 	files, err := listFiles(inputDir, startIndex)
