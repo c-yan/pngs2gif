dccd204c86582521024a5a2e0adf4bb8a952c152
c-yan <c-yan@users.noreply.github.com>
2018-07-23 13:17:02 +0000

    Fixed wrong RGB shift size

--- quantize.go
+++ quantize.go
@@ -12,7 +12,7 @@ func collectHistogram(i image.Image) {
 	for y := i.Bounds().Min.Y; y < i.Bounds().Max.Y; y++ {
 		for x := i.Bounds().Min.X; x < i.Bounds().Max.X; x++ {
 			r, g, b, _ := i.At(x, y).RGBA()
-			histogram[r>>16][g>>16][b>>16]++
+			histogram[r>>8][g>>8][b>>8]++
 		}
 	}
 }
